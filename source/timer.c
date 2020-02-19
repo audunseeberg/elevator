a1e064d997ea16b1d083c19efd920c7544c48e32
Audun Seeberg <audunsse@stud.ntnu.no>
2020-02-19 17:34:10 +0100

    changed some stop-functionality

--- source/timer.c
+++ source/timer.c
@@ -14,23 +14,14 @@ void set_timer_duration(int time_in_ms){
 int startTimer(){
     int time_passed = 0;
     clock_t start_time = clock();
-    bool stop_active = 0;
-
     do {
         clock_t time_diff = clock() - start_time;
         time_passed = time_diff * 1000 / CLOCKS_PER_SEC;
-        if(hardware_read_stop_sigal()){
-            stop_active = 1;
-        }
-        if (hardware_obstruction_signal()){
+        if (hardware_obstruction_signal() || hardware_read_stop_sigal()){
             return 0;
         }
 
     } while (time_passed < timer_duration);
-    
-    if (stop_active){
-        stop_procedure();
-    }
 
     return 0;
 };
--- source/timer.h
+++ source/timer.h
@@ -14,8 +14,9 @@ void set_timer_duration(int time_in_ms);
 
 /**
  * @brief Starts the timer and lets it run for the set timer duration unless the
- * obstruction button is pushed. Runs stop-procedure if stop button is pushed during.
+ * obstruction button or stop button is pushed. 
  * 
- * @return 0 if the obstruction button is pushed. 1 otherwise (after timer_duration milliseconds).
+ * @return 0 if the obstruction button or stop button is pushed. 
+ * 1 otherwise (after timer_duration milliseconds).
  */
 int start_timer();
