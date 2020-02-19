64fe6d6fff38b23986e16a51caf230c6bf2a29d5
Audun Seeberg <audunsse@stud.ntnu.no>
2020-02-19 16:50:19 +0100

    doxygen documentation and obstruction/stop added

--- source/timer.c
+++ source/timer.c
@@ -4,21 +4,33 @@
 #include <time.h>
 #include "timer.h"
 
-int timer_length;
+int timer_duration;
 
-void setTimerLength(int millisecs){
-    timer_length = millisecs;
+void set_timer_duration(int time_in_ms){
+    timer_duration = time_in_ms;
 }
 
 
 int startTimer(){
     int time_passed = 0;
     clock_t start_time = clock();
+    bool stop_active = 0;
 
     do {
         clock_t time_diff = clock() - start_time;
         time_passed = time_diff * 1000 / CLOCKS_PER_SEC;
-    } while (time_passed < timer_length);
+        if(hardware_read_stop_sigal()){
+            stop_active = 1;
+        }
+        if (hardware_obstruction_signal()){
+            return 0;
+        }
+
+    } while (time_passed < timer_duration);
     
-    return 1;
+    if (stop_active){
+        stop_procedure();
+    }
+
+    return 0;
 };
--- source/timer.h
+++ source/timer.h
@@ -1,4 +1,21 @@
-#pragma once
+/**
+ * @file
+ * 
+ * @brief Timer functionality.
+ * 
+ */
 
-void setTimerLength();
-int startTimer();
+/**
+ * @brief Sets the duration of the timer.
+ * 
+ * @param time_in_ms Timer duration in milliseconds.
+ */
+void set_timer_duration(int time_in_ms);
+
+/**
+ * @brief Starts the timer and lets it run for the set timer duration unless the
+ * obstruction button is pushed. Runs stop-procedure if stop button is pushed during.
+ * 
+ * @return 0 if the obstruction button is pushed. 1 otherwise (after timer_duration milliseconds).
+ */
+int start_timer();
