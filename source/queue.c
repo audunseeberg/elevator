bff0dc548d16285764879986387af8fe53d7aa7a
Audun Seeberg <audunsse@stud.ntnu.no>
2020-02-20 16:44:32 +0100

    various implementations

--- source/queue.c
+++ source/queue.c
@@ -2,15 +2,37 @@
 #include <stdbool.h>
 #include "queue.h"
 
+int orders[10] = {-1};
+
+int order_to_int_encoding(int floor, HardwareOrder type){
+    return (floor-1)*3+(int)type;
+}
+
 void add_order(int floor, HardwareOrder type){
 
 }
 
 void del_all_orders_on_floor(int floor){
-
+    for (int i = 0; i < sizeof(orders); i++){
+        for (int j = 0; j < 3; j++){
+            if (orders[i] = order_to_int_encoding(floor, j)){
+                orders[i] = -1;
+            }
+        }
+    }
+    int next_in_queue = 0;                                          //restacks the queue to place valid orders first
+    int array_size = (sizeof(orders)/sizeof(orders[0]));
+    for (int c = 0; c < array_size; c++){
+        if (orders[c] != -1){
+            orders[next_in_queue++] = orders[c];
+        }  
+    }
+    while (next_in_queue < array_size) {
+        orders[next_in_queue++] = -1;
+    }
 }
 
-int check_queue_for_order(int floor, HardwareOrder type){
+bool check_queue_for_order(int floor, HardwareOrder type){
 
 }
 
@@ -18,6 +40,20 @@ bool check_for_stop(int floor, HardwareMovement direction){
 
 }
 
-int check_queue_length(){
+bool check_if_queue_empty(){
 
+}
+
+int main(){
+    int o[10] = {0, 8, 5, 6, 4, -1, 3, 7, -1, 10};
+    int next_in_queue = 0;
+    int array_size = (sizeof(o)/sizeof(o[0]));
+    for (int c = 0; c < array_size; c++){
+        if (o[c] != -1){
+            o[next_in_queue++] = o[c];
+        }  
+    }
+    while (next_in_queue < array_size) {
+        o[next_in_queue++] = -1;
+    }
 }
--- source/queue.h
+++ source/queue.h
@@ -1,16 +1,57 @@
+/**
+ * @file
+ * @brief Implements queue system for orders
+ */
 #include "hardware.h"
 
-struct order {
-    HardwareOrder orderType;
-    int floor;
-};
+/**
+ * @brief Encodes order to corresponding int
+ * 
+ * @param floor Floor to be encoded
+ * @param type Order type to be encoded
+ * @return Order as an integer.
+ */
+int order_to_int_encoding(int floor, HardwareOrder type);
 
+/**
+ * @brief Adds an order to the queue
+ * 
+ * @param floor Floor to add
+ * @param type Order type to add
+ */
 void add_order(int floor, HardwareOrder type);
 
+/**
+ * @brief Deletes all orders in the queue on specified floor.
+ * 
+ * @param floor Floor to delete orders on.
+ */
 void del_all_orders_on_floor(int floor);
 
-int check_queue_for_order(int floor, HardwareOrder type);
+/**
+ * @brief Checks if a specified order is in the queue.
+ * 
+ * @param floor Floor to look for
+ * @param type Order type to look for
+ * @return 1 if order in queue. 0 otherwise 
+ */
+bool check_queue_for_order(int floor, HardwareOrder type);
 
+
+
+/**
+ * @brief Checks if there are orders in the queue requiring the elevator to stop.
+ * 
+ * @param floor Current elevator floor
+ * @param direction Current elevator direction
+ * @return 1 if elevator should stop. 0 otherwise.
+ */
 bool check_for_stop(int floor, HardwareMovement direction);
 
-int check_queue_length();
+
+/**
+ * @brief Checks if the queue has any valid orders
+ * 
+ * @return 1 if queue empty. 0 otherwise.
+ */
+bool check_if_queue_empty();
