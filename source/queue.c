a4a7213283719946d72f95c0ff479dacd93b4639
Audun Seeberg <audunsse@stud.ntnu.no>
2020-02-20 18:12:15 +0100

    moved del_all_orders to queue and fixed some bugs

--- source/queue.c
+++ source/queue.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "queue.h"
-int orders[10];
 
-int orders[10] = {-1};
+//int orders[10] = {0, 5, 4, 8, 1, 7, 11, 10, -1, -1};
+int orders[10] = { [0 ... 9] =-1 };
 int array_size = (sizeof(orders)/sizeof(orders[0]));
 
 int order_to_int_encoding(int floor, HardwareOrder type){
@@ -11,12 +11,14 @@ int order_to_int_encoding(int floor, HardwareOrder type){
 }
 
 void add_order(int floor, HardwareOrder type){
-	for (int i = 9;i > -1;i--) {
-		if (orders[i] != -1) {
-			orders[i + 1] = (((floor-1) * 3) + (int)type);
-		}
+    if (!check_queue_for_order(floor, type)) {
+        for (int i = 0; i < array_size; i++) {
+            if (orders[i] == -1){
+			    orders[i] = order_to_int_encoding(floor, type);
+                break;
+            }
+        }
 	}
-
 }
 
 void del_all_orders_on_floor(int floor){
@@ -28,7 +30,6 @@ void del_all_orders_on_floor(int floor){
         }
     }
     int next_in_queue = 0;                                          //restacks the queue to place valid orders first
-    int array_size = (sizeof(orders)/sizeof(orders[0]));
     for (int c = 0; c < array_size; c++){
         if (orders[c] != -1){
             orders[next_in_queue++] = orders[c];
@@ -39,6 +40,13 @@ void del_all_orders_on_floor(int floor){
     }
 }
 
+void del_all_orders() {
+    for (int i = 0; i < array_size; i++){
+        orders[i] = -1;
+    }
+}
+
+
 bool check_queue_for_order(int floor, HardwareOrder type){
     int order_int = order_to_int_encoding(floor, type);
     for (int i = 0;i < 10;i++) {
@@ -46,7 +54,7 @@ bool check_queue_for_order(int floor, HardwareOrder type){
             return 1;
         }
     }
-
+    return 0;
 }
 
 bool check_for_stop(int floor, HardwareMovement direction){
